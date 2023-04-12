0fe3fc759b0c95b45004388659e96dc6120cdf72
Camila Locaso <caami.94@hotmail.com>
2023-04-12 00:12:11 -0300

    vamos devuelta

---
+++ JavaIntro01.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaintro01;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author caami
+ */
+public class JavaIntro01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner leer = new Scanner(System.in);
+        
+        String nombre;
+        
+        System.out.println("Ingresa tu nombre");
+        
+        nombre = leer.next();
+       
+        System.out.println("Hola Mundo! Soy " + nombre + " y estoy programando en Java!");
+        
+        
+        // A partir de ahora empiezan los ejercicios de la Guia 1 de Java
+        //1- Deteccion de errores
+        
+        //public static void main(String[] args) {}
+        //Lo de arriba iria pero como yo sigo en el mismo proyecto estaria repetido
+  	//Scanner leer = new Scanner (System.in);
+        //String nombre; 
+        // Lo de arriba iria pero como yo sigo en el mismo proyecto estaria repetido
+  	boolean bandera;
+  	char niIdea; 
+        
+        //2- Asignale un valor a las variables del ejercicio anterior
+        
+        
+        boolean bandera = false ;
+  	char niIdea = 0; 
+        
+        // Deteccion de errores 2
+          
+
+          int numero = 48;
+          double decimales = 3.55; 	
+          boolean bandera 
+        
+    }
+    
+}
